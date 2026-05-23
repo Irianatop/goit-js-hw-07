169ed32b9b38146fd901651a33e94f0f35d2c302
Iriana <irichan24@gmail.com>
2026-05-23 22:34:18 +0100

    update js and css

--- js/task-2.js
+++ js/task-2.js
@@ -24,3 +24,14 @@ const images = [
     alt: "Lighthouse Coast Sea",
   }
 ];
+
+const gallery = document.querySelector('.gallery');
+const markup = images
+  .map(
+    image =>
+      `< class="gallery-item">
+    <img src="${image.url}" alt="${image.alt}" width="300">
+  </li>`
+  )
+  .join("");
+gallery.insertAdjacentHTML("beforeend", markup);
